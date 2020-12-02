097b197687bc209194f21b28d1fdb2b31f7d51e5
Amir Kalmoni <amirkalmoni@Amirs-MacBook-Pro.local>
2020-12-02 02:14:37 -0500

    app UI has been added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Header from './components/Header'
 import Balance from './components/Balance'
 import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList'
+import AddTransaction from './components/AddTransaction';
 
 function App() {
   return (
@@ -9,8 +11,11 @@ function App() {
       <Header/>
       <div className = "container">
         <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
       </div>
-      <IncomeExpenses/>
+      
     </div>
   );
 }
